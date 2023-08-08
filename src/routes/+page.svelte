34631833569150fe357ac2a61427782ad370686c
henry <henryhu712@gmail.com>
2023-08-08 13:40:47 +0800

    logo, favicon, title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,22 @@
+
+<svelte:head>
+  <title>Home | aishell.io</title>
+</svelte:head>
+
+
+<div class="flex flex-col h-screen justify-between">
+  <nav class="h-12">
+    <div class="container mx-auto px-6 py-2 flex justify-between items-center">
+      <a class="font-bold text-2xl lg:text-2xl" href="#">
+		<img src="/logo.png" alt="logo" class="" />
+      </a>
+    </div>
+  </nav>
+
+  <div class="mb-auto h-10">
 <h1 class="text-3xl underline">
 	Welcome to SvelteKit
 </h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+</div>
