92a66b0d1007d18f34c5e80b03b6521776274249
henry <henryhu712@gmail.com>
2023-09-04 15:02:04 +0800

    slogan

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,21 @@
 
-<svelte:head>
-  <title>Home | aishell.io</title>
-</svelte:head>
+  <svelte:head>
+    <title>Home | aishell.io</title>
+  </svelte:head>
 
 
-<div class="flex flex-col h-screen justify-between">
-  <nav class="h-12">
-    <div class="container mx-auto px-6 py-2 flex justify-between items-center">
-      <a class="font-bold text-2xl lg:text-2xl" href="#">
-		<img src="/logo.png" alt="logo" class="" />
-      </a>
+  <div class="py-20" style="background: linear-gradient(90deg, #667eea 0%, #764ba2 100%)" >
+    <div class="container mx-auto px-6">
+      <h2 class="text-4xl font-bold mb-2 text-white">
+        Manage your chat with ChatGPT easily.
+      </h2>
+      <h3 class="text-2xl mb-8 text-gray-200">
+        A chat management system.
+      </h3>
+
+      <button class="bg-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider">
+        Get started
+      </button>
     </div>
-  </nav>
+  </div>
 
-  <div class="mb-auto h-10">
-<h1 class="text-3xl underline">
-	Welcome to SvelteKit
-</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-</div>
-</div>
